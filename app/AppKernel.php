0180a22e61b1212ee561349cb5c1b600aa06afb0
Matthieu Moquet <matthieu@moquet.net>
2014-12-01 22:14:22 +0100

    Add JMSTranslationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,6 +14,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
             new AppBundle\AppBundle(),
         );
 
